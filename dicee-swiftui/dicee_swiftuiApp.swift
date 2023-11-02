90c50514feaff27ab2e534c236a8a9f80a690c6e
Raja Shravan <raja@banter.shopping>
2023-11-02 16:07:29 -0400

    Initial Commit

---
+++ dicee-swiftui/dicee_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_swiftuiApp.swift
+//  dicee-swiftui
+//
+//  Created by Raja Shravan on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct dicee_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
